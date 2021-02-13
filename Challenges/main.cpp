e9078f2c11a866c57fba7b39ea7c5d3b7f654360
Chris <ghriscarcia@gmail.com>
2021-02-13 16:42:58 -0600

    Initial Commit

---
+++ Challenges/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Challenges
+//
+//  Created by Chris G on 2/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
